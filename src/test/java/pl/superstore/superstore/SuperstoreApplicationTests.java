8f63f2f92c9747fe4bb0b2fa9d5d1654bd5d168d
Melak <km250542@ncr.com>
2023-03-31 12:22:51 +0200

    init

---
+++ src/test/java/pl/superstore/superstore/SuperstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.superstore.superstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
